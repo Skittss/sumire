9e37a4285d58c5bafa6cc81312eecc0f4130a4cb
Skitts <hennoallen@hotmail.co.uk>
2024-02-26 16:22:46 +0000

    fixed indentation

--- src/sumire/core/sumi_descriptors.hpp
+++ src/sumire/core/sumi_descriptors.hpp
@@ -8,41 +8,41 @@
  
 namespace sumire {
  
-class SumiDescriptorSetLayout {
-	public:
+	class SumiDescriptorSetLayout {
+		public:
 
-		class Builder {
-			public:
-				Builder(SumiDevice &sumiDevice) : sumiDevice{sumiDevice} {}
-			
-				Builder &addBinding(
-					uint32_t binding,
-					VkDescriptorType descriptorType,
-					VkShaderStageFlags stageFlags,
-					uint32_t count = 1);
-				std::unique_ptr<SumiDescriptorSetLayout> build() const;
-			
-			private:
-				SumiDevice &sumiDevice;
-				std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings{};
-		};
-	
-		SumiDescriptorSetLayout(
-				SumiDevice &sumiDevice, std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings);
-		~SumiDescriptorSetLayout();
-		SumiDescriptorSetLayout(const SumiDescriptorSetLayout &) = delete;
-		SumiDescriptorSetLayout &operator=(const SumiDescriptorSetLayout &) = delete;
+			class Builder {
+				public:
+					Builder(SumiDevice &sumiDevice) : sumiDevice{sumiDevice} {}
+				
+					Builder &addBinding(
+						uint32_t binding,
+						VkDescriptorType descriptorType,
+						VkShaderStageFlags stageFlags,
+						uint32_t count = 1);
+					std::unique_ptr<SumiDescriptorSetLayout> build() const;
+				
+				private:
+					SumiDevice &sumiDevice;
+					std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings{};
+			};
 		
-		VkDescriptorSetLayout getDescriptorSetLayout() const { return descriptorSetLayout; }
-	
-	private:
-		SumiDevice &sumiDevice;
-		VkDescriptorSetLayout descriptorSetLayout;
-		std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings;
+			SumiDescriptorSetLayout(
+					SumiDevice &sumiDevice, std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings);
+			~SumiDescriptorSetLayout();
+			SumiDescriptorSetLayout(const SumiDescriptorSetLayout &) = delete;
+			SumiDescriptorSetLayout &operator=(const SumiDescriptorSetLayout &) = delete;
+			
+			VkDescriptorSetLayout getDescriptorSetLayout() const { return descriptorSetLayout; }
 		
-		friend class SumiDescriptorWriter;
+		private:
+			SumiDevice &sumiDevice;
+			VkDescriptorSetLayout descriptorSetLayout;
+			std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings;
+			
+			friend class SumiDescriptorWriter;
 	};
-	
+		
 	class SumiDescriptorPool {
 		public:
 
@@ -60,46 +60,46 @@ class SumiDescriptorSetLayout {
 					std::vector<VkDescriptorPoolSize> poolSizes{};
 					uint32_t maxSets = 1000;
 					VkDescriptorPoolCreateFlags poolFlags = 0;
-			};
+				};
+
+			SumiDescriptorPool(
+				SumiDevice &sumiDevice,
+				uint32_t maxSets,
+				VkDescriptorPoolCreateFlags poolFlags,
+				const std::vector<VkDescriptorPoolSize> &poolSizes);
+			~SumiDescriptorPool();
+			SumiDescriptorPool(const SumiDescriptorPool &) = delete;
+			SumiDescriptorPool &operator=(const SumiDescriptorPool &) = delete;
+			
+			bool allocateDescriptorSet(
+				const VkDescriptorSetLayout descriptorSetLayout, VkDescriptorSet &descriptor) const;
+			
+			void freeDescriptorSet(std::vector<VkDescriptorSet> &descriptors) const;
+			
+			void resetPool();
+
+		private:
+			SumiDevice &sumiDevice;
+			VkDescriptorPool descriptorPool;
+			
+			friend class SumiDescriptorWriter;
+	};
 	
-		SumiDescriptorPool(
-			SumiDevice &sumiDevice,
-			uint32_t maxSets,
-			VkDescriptorPoolCreateFlags poolFlags,
-			const std::vector<VkDescriptorPoolSize> &poolSizes);
-		~SumiDescriptorPool();
-		SumiDescriptorPool(const SumiDescriptorPool &) = delete;
-		SumiDescriptorPool &operator=(const SumiDescriptorPool &) = delete;
-		
-		bool allocateDescriptorSet(
-			const VkDescriptorSetLayout descriptorSetLayout, VkDescriptorSet &descriptor) const;
-		
-		void freeDescriptorSet(std::vector<VkDescriptorSet> &descriptors) const;
-		
-		void resetPool();
-	
-	private:
-		SumiDevice &sumiDevice;
-		VkDescriptorPool descriptorPool;
-		
-		friend class SumiDescriptorWriter;
-};
- 
-class SumiDescriptorWriter {
+	class SumiDescriptorWriter {
 
-	public:
-		SumiDescriptorWriter(SumiDescriptorSetLayout &setLayout, SumiDescriptorPool &pool);
+		public:
+			SumiDescriptorWriter(SumiDescriptorSetLayout &setLayout, SumiDescriptorPool &pool);
 
-		SumiDescriptorWriter &writeBuffer(uint32_t binding, VkDescriptorBufferInfo *bufferInfo);
-		SumiDescriptorWriter &writeImage(uint32_t binding, VkDescriptorImageInfo *imageInfo);
+			SumiDescriptorWriter &writeBuffer(uint32_t binding, VkDescriptorBufferInfo *bufferInfo);
+			SumiDescriptorWriter &writeImage(uint32_t binding, VkDescriptorImageInfo *imageInfo);
 
-		bool build(VkDescriptorSet &set);
-		void overwrite(VkDescriptorSet &set);
+			bool build(VkDescriptorSet &set);
+			void overwrite(VkDescriptorSet &set);
 
-	private:
-		SumiDescriptorSetLayout &setLayout;
-		SumiDescriptorPool &pool;
-		std::vector<VkWriteDescriptorSet> writes;
+		private:
+			SumiDescriptorSetLayout &setLayout;
+			SumiDescriptorPool &pool;
+			std::vector<VkWriteDescriptorSet> writes;
 	};
  
 }
