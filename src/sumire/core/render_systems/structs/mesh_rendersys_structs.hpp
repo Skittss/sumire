7dabf5630017b9d4803d077156d96cafb63e679a
Henry Allen <skittss@outlook.com>
2024-03-02 19:37:25 +0000

    Removed comment - extra namespace level already given

--- src/sumire/core/render_systems/structs/mesh_rendersys_structs.hpp
+++ src/sumire/core/render_systems/structs/mesh_rendersys_structs.hpp
@@ -2,8 +2,6 @@
 
 #include <glm/glm.hpp>
 
-// Note: An extra namespace level here may alleviate potential struct mix-ups if there are multiple
-//       with the same name in different hpp files.
 namespace sumire::structs {
 
     struct VertPushConstantData {
