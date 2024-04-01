9be1484bb6e5732abefb8728ff8fc03a0fa10ddf
Henry Allen <skittss@outlook.com>
2024-04-01 01:37:00 +0100

    More accurate sRGB to linear RGB conversion

--- shaders/includes/srgb2linear.glsl
+++ shaders/includes/srgb2linear.glsl
@@ -1,4 +1,4 @@
 vec4 srgb2linear(vec4 srgb) {
-    vec3 linear = pow(srgb.rgb, vec3(2.2));
-    return vec4(linear, srgb.a);
+	vec3 bLess = step(vec3(0.04045), srgb.xyz);
+	vec3 linOut = mix(srgb.xyz / vec3(12.92), pow((srgb.xyz + vec3(0.055))/vec3(1.055), vec3(2.4)), bLess);
 }
