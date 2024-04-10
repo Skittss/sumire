afc799c40daa1ef323aaa1b91c81bc41ed3d27c9
Henry Allen <skittss@outlook.com>
2024-04-10 22:16:31 +0100

    Added vulkan error message output to VK_CHECK_SUCCESS macro

--- src/sumire/util/vk_check_success.hpp
+++ src/sumire/util/vk_check_success.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
 
 #include <stdexcept>
+#include <string>
 
-#define VK_CHECK_SUCCESS(result, error_msg)     \
-{                                               \
-    VkResult res = (result);                    \
-    if (res != VK_SUCCESS) {                    \
-        throw std::runtime_error((error_msg));  \
-    }                                           \
+#define VK_CHECK_SUCCESS(result, error_msg)                                             \
+{                                                                                       \
+    VkResult res = (result);                                                            \
+    if (res != VK_SUCCESS) {                                                            \
+        std::string vk_error_msg = string_VkResult(res);                                \
+        std::string user_error_msg = (error_msg);                                       \
+        throw std::runtime_error(user_error_msg + " [VkResult: " + vk_error_msg + "]"); \
+    }                                                                                   \
 }
