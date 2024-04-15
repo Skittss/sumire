937ce00dc075b55c570738fb20bb9f3ef6c76813
Henry Allen <skittss@outlook.com>
2024-04-15 04:04:51 +0100

    zBin container for metadata and helper funcs

--- src/sumire/core/render_systems/data_structs/high_quality_shadow_mapper_structs.hpp
+++ src/sumire/core/render_systems/data_structs/high_quality_shadow_mapper_structs.hpp
@@ -5,6 +5,7 @@
 #include <glm/glm.hpp>
 
 #include <cstdint>
+#include <vector>
 
 namespace sumire::structs {
 
@@ -12,6 +13,8 @@ namespace sumire::structs {
 		SumiLight* lightPtr = nullptr;
 		glm::vec3 viewSpacePosition{ 0.0f };
 		float viewSpaceDepth = 0.0f;
+		float minDepth = 0.0f;
+		float maxDepth = 0.0f;
 	};
 
 	// Combination of Zbin and Ranged Zbin.
@@ -22,6 +25,27 @@ namespace sumire::structs {
 		int maxLightIdx = -1;
 		int rangedMinLightIdx = -1;
 		int rangedMaxLightIdx = -1;
+	};
+
+	struct zBin {
+		int minLight = -1;
+		int maxLight = -1;
+		int firstFullIdx = -1;
+		int lastFullIdx = -1;
+
+		std::vector<zBinData> data;
+
+		zBin(const uint32_t size) {
+			data = std::vector<zBinData>(size);
+		}
+
+		void reset() {
+			std::fill(data.begin(), data.end(), zBinData{});
+			firstFullIdx = -1;
+			lastFullIdx = -1;
+			minLight = -1;
+			maxLight = -1;
+		}
 	};
 
 }
