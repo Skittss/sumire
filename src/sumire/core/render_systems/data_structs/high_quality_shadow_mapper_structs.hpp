e7f3b896946c24f1e1b75d4c5c797ea6718ea5c6
Henry Allen <skittss@outlook.com>
2024-04-17 19:43:36 +0100

    Light mask bit fields and updated lightMask for cleaner data access

--- src/sumire/core/render_systems/data_structs/high_quality_shadow_mapper_structs.hpp
+++ src/sumire/core/render_systems/data_structs/high_quality_shadow_mapper_structs.hpp
@@ -17,17 +17,76 @@ namespace sumire::structs {
 		float maxDepth = 0.0f;
 	};
 
+	// We send light mask bits to a shader as vectors of uint32_t
 	struct lightMaskBitField {
-	
+		long bits : 32;
+	};
+
+	struct lightMaskTile {
+		// Idx 0: Reserved for light groups
+		// Idx 1-32: Light mask bits
+		lightMaskBitField bitFields[33] { 0b0 };
+
+		void clear() {
+			for (auto& field : bitFields)
+				field.bits = 0b0;
+		}
+
+		bool isBitSet(uint32_t x, uint32_t y) const {
+			assert(x < 32 && y < 33 && "queried bit index is out of range");
+			return bitFields[y].bits & x;
+		}
+
+		void setLightBit(uint32_t lightIdx) {
+			uint32_t lightGroupIdx = lightIdx / 32u;
+
+			// Set light bit and light group bit
+			bitFields[0].bits |= lightGroupIdx;
+
+			uint32_t bitIdx = lightIdx - 32u * lightGroupIdx;
+			bitFields[lightGroupIdx + 1].bits |= bitIdx;
+		}
 	};
 
 	struct lightMask {
 		// Idx 0 is reserved for light *groups* for speeding up iteration
 		//  of this buffer.
-		std::vector<lightMaskBitField> bits;
 
-		lightMask(const uint32_t tileX, const uint32_t tileY) {
-			bits = std::vector<lightMaskBitField>(tileY);
+		const uint32_t width;
+		const uint32_t height;
+		const uint32_t numTilesX;
+		const uint32_t numTilesY;
+
+		std::vector<lightMaskTile> tiles;
+
+		void clear() {
+			// clear bits of all tiles
+			for (auto& tile : tiles)
+				tile.clear();
+		}
+
+		const lightMaskTile& readTileAtIdx(const uint32_t x, const uint32_t y) const {
+			return tiles[x + y * numTilesX];
+		}
+
+		lightMaskTile& tileAtIdx(const uint32_t x, const uint32_t y) { 
+			return tiles[x + y * numTilesX];
+		}
+
+		lightMaskTile& tileAtRasterPos(const uint32_t x, const uint32_t y) {
+			uint32_t xIdx = x - (x / 32u);
+			uint32_t yIdx = y - (y / 32u);
+
+			return tiles[x + y * numTilesX];
+		}
+
+		lightMask(
+			const uint32_t width, const uint32_t height
+		) : width{ width }, height{ height }, 
+			numTilesX{ width / 32u }, numTilesY{ height / 32u } 
+		{
+
+			tiles = std::vector<lightMaskTile>(numTilesX * numTilesY);
 		}
 	};
 
