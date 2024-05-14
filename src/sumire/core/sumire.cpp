06742105b933a660b4fb6f137c9b6088b2cdcca6
Henry Allen <skittss@outlook.com>
2024-05-14 22:36:43 +0100

    Fixed global descriptors not being available in compute stages

--- src/sumire/core/sumire.cpp
+++ src/sumire/core/sumire.cpp
@@ -109,9 +109,9 @@ namespace sumire {
             .build();
 
         globalDescriptorSetLayout = SumiDescriptorSetLayout::Builder(sumiDevice)
-            .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
-            .addBinding(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
-            .addBinding(2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
+            .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT)
+            .addBinding(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT)
+            .addBinding(2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT)
             .build();
 
         // Write Descriptor Sets
