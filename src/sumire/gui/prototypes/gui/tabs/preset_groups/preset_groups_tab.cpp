d0c363250d815cebc8a6319e972c96e52201834e
Skittss <skittss@outlook.com>
2025-07-28 23:55:30 +0100

    Pull preset groups from disk

--- src/sumire/gui/prototypes/gui/tabs/preset_groups/preset_groups_tab.cpp
+++ src/sumire/gui/prototypes/gui/tabs/preset_groups/preset_groups_tab.cpp
@@ -1,7 +1,10 @@
 #include <sumire/gui/prototypes/gui/tabs/preset_groups/preset_groups_tab.hpp>
 
 #include <sumire/gui/prototypes/gui/tabs/shared/styling_consts.hpp>
+#include <sumire/gui/prototypes/gui/tabs/shared/alignment.hpp>
+#include <sumire/gui/prototypes/data/ids/font_symbols.hpp>
 #include <sumire/gui/prototypes/data/formats/preset_group.hpp>
+#include <sumire/gui/prototypes/util/string/to_lower.hpp>
 
 #include <vector>
 #include <algorithm>
@@ -14,101 +17,172 @@ namespace kbf {
         // TODO: Func this
         const ImVec2 buttonSize = ImVec2(ImGui::GetContentRegionAvail().x, 0.0f);
         if (ImGui::Button("Create Preset Group", buttonSize)) {
-
+            openCreatePresetGroupPanel();
         }
 
-        if (ImGui::Button("Create From Active FBS Presets", buttonSize)) {
+        if (ImGui::Button("Create From Preset Bundle", buttonSize)) {
 
         }
         ImGui::Spacing();
 
-        static std::vector<PresetGroup> presetGroups = {
-            //         { "Preset Group 1",  {} },
-            //         { "Preset Group 2",  { {"Bruh", {"Bruh", "Armour 1"} } } },
-            //         { "Preset Group 3",  { {"Bruh", {"Bruh", "Armour 1"} }, {"Bruh2", {"Bruh", "Armour 1"} } } } ,
-            //         { "Preset Group 4",  {} },
-            //         { "Preset Group 5",  {} },
-            //         { "Preset Group 6",  {} },
-            //         { "Preset Group 7",  {} },
-            //         { "Preset Group 8",  {} },
-            //         { "Preset Group 9",  {} },
-                     //{ "Preset Group 10", {} }
-        };
+        drawPresetGroupList();
 
-        constexpr ImGuiTableFlags presetGroupTableFlags =
-            ImGuiTableFlags_BordersInnerH
-            | ImGuiTableFlags_BordersInnerV
-            | ImGuiTableFlags_PadOuterX
-            | ImGuiTableFlags_Sortable;
-        ImGui::BeginTable("##PresetGroupList", 1, presetGroupTableFlags);
+        ImGui::PopStyleVar();
+	}
 
-        constexpr ImGuiTableColumnFlags stretchSortFlags =
-            ImGuiTableColumnFlags_DefaultSort | ImGuiTableColumnFlags_WidthStretch;
+	void PresetGroupsTab::drawPopouts() {
+        createPresetGroupPanel.draw();
+        editPresetGroupPanel.draw();
+    };
 
-        ImGui::TableSetupColumn("Preset Group", stretchSortFlags, 0.0f);
-        ImGui::TableHeadersRow();
+    void PresetGroupsTab::drawPresetGroupList() {
+        static bool sortDirAscending;
+        static enum class SortCol {
+            NONE,
+            NAME
+        } sortCol;
 
-        // Sorting for preset group name
-        if (ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs()) {
-            if (sort_specs->SpecsDirty && sort_specs->SpecsCount > 0) {
-                const ImGuiTableColumnSortSpecs& sort_spec = sort_specs->Specs[0];
+        std::vector<const PresetGroup*> presetGroups = dataManager.getPresetGroups("");
 
-                std::sort(presetGroups.begin(), presetGroups.end(), [&](const PresetGroup& a, const PresetGroup& b) {
-                    if (a.data && !b.data) return true;
-                    if (!a.data && b.data) return false;
-                    if (!a.data || !b.data) return false;
+        // Sort
+        switch (sortCol)
+        {
+        case SortCol::NAME:
+            std::sort(presetGroups.begin(), presetGroups.end(), [&](const PresetGroup* a, const PresetGroup* b) {
+                std::string lowa = toLower(a->name); std::string lowb = toLower(b->name);
+                return sortDirAscending ? lowa < lowb : lowa > lowb;
+            });
+        }
+
+        if (presetGroups.size() == 0) {
+            ImGui::Spacing();
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
+            constexpr char const* noPresetStr = "No Existing Preset Groups";
+            preAlignCellContentHorizontal(noPresetStr);
+            ImGui::Text(noPresetStr);
+            ImGui::PopStyleColor();
+        }
+        else {
+            constexpr ImGuiTableFlags presetGroupTableFlags =
+                ImGuiTableFlags_BordersInnerH
+                | ImGuiTableFlags_BordersInnerV
+                | ImGuiTableFlags_PadOuterX
+                | ImGuiTableFlags_Sortable;
+            ImGui::BeginTable("##PresetGroupList", 1, presetGroupTableFlags);
+
+            constexpr ImGuiTableColumnFlags stretchSortFlags =
+                ImGuiTableColumnFlags_DefaultSort | ImGuiTableColumnFlags_WidthStretch;
+
+            ImGui::TableSetupColumn("Preset Group", stretchSortFlags, 0.0f);
+            ImGui::TableHeadersRow();
+
+            // Sorting for preset group name
+            if (ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs()) {
+                if (sort_specs->SpecsDirty && sort_specs->SpecsCount > 0) {
+                    const ImGuiTableColumnSortSpecs& sort_spec = sort_specs->Specs[0];
+                    sortDirAscending = sort_spec.SortDirection == ImGuiSortDirection_Ascending;
 
                     switch (sort_spec.ColumnIndex)
                     {
-                    case 0: return (sort_spec.SortDirection == ImGuiSortDirection_Ascending) ? (a.data->name < b.data->name) : (a.data->name > b.data->name);
-                    default: return false;
+                    case 0:  sortCol = SortCol::NAME; break;
+                    default: sortCol = SortCol::NONE; break;
                     }
-                    });
 
-                sort_specs->SpecsDirty = false;
+                    sort_specs->SpecsDirty = false;
+
+                }
             }
+
+            ImGui::PopStyleVar();
+            ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(LIST_PADDING.x, 0.0f));
+
+            for (const PresetGroup* group : presetGroups) {
+                ImGui::TableNextRow();
+
+                ImGui::TableNextColumn();
+
+                constexpr float selectableHeight = 60.0f;
+                ImVec2 pos = ImGui::GetCursorScreenPos();
+                if (ImGui::Selectable(("##Selectable_" + group->name).c_str(), false, 0, ImVec2(0.0f, selectableHeight))) {
+                    openEditPresetGroupPanel(group->uuid);
+                }
+
+                // Sex Mark
+                std::string sexMarkSymbol = group->female ? WS_FONT_FEMALE : WS_FONT_MALE;
+                ImVec4 sexMarkerCol = group->female ? ImVec4(0.76f, 0.50f, 0.24f, 1.0f) : ImVec4(0.50f, 0.70f, 0.33f, 1.0f);
+
+                ImGui::PushFont(wsSymbolFont);
+
+                constexpr float sexMarkerSpacingAfter = 5.0f;
+                constexpr float sexMarkerVerticalAlignOffset = 5.0f;
+                ImVec2 sexMarkerSize = ImGui::CalcTextSize(sexMarkSymbol.c_str());
+                ImVec2 sexMarkerPos;
+                sexMarkerPos.x = pos.x + ImGui::GetStyle().ItemSpacing.x;
+                sexMarkerPos.y = pos.y + (selectableHeight - sexMarkerSize.y) * 0.5f + sexMarkerVerticalAlignOffset;
+                ImGui::GetWindowDrawList()->AddText(sexMarkerPos, ImGui::GetColorU32(sexMarkerCol), sexMarkSymbol.c_str());
+
+                ImGui::PopFont();
+
+                // Group name... floating because imgui has no vertical alignment STILL :(
+                constexpr float groupNameSpacingAfter = 5.0f;
+                ImVec2 groupNameSize = ImGui::CalcTextSize(group->name.c_str());
+                ImVec2 groupNamePos;
+                groupNamePos.x = sexMarkerPos.x + sexMarkerSize.x + sexMarkerSpacingAfter;
+                groupNamePos.y = pos.y + (selectableHeight - groupNameSize.y) * 0.5f;
+                ImGui::GetWindowDrawList()->AddText(groupNamePos, ImGui::GetColorU32(ImGuiCol_Text), group->name.c_str());
+
+                // Similarly for preset count
+                const size_t nPresets = group->presets.size();
+                std::string rightText = std::to_string(nPresets);
+                if (nPresets == 0) rightText = "Empty";
+                else if (nPresets == 1) rightText += " Preset";
+                else rightText += " Presets";
+
+                ImVec2 rightTextSize = ImGui::CalcTextSize(rightText.c_str());
+                float contentRegionWidth = ImGui::GetContentRegionAvail().x;
+                float cursorPosX = ImGui::GetCursorScreenPos().x + contentRegionWidth - rightTextSize.x - ImGui::GetStyle().ItemSpacing.x;
+
+                ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
+                ImGui::GetWindowDrawList()->AddText(ImVec2(cursorPosX, groupNamePos.y), ImGui::GetColorU32(ImGuiCol_Text), rightText.c_str());
+                ImGui::PopStyleColor();
+            }
+
+            ImGui::EndTable();
+            ImGui::PopStyleVar(1);
         }
+    }
 
-        ImGui::PopStyleVar();
-        ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(LIST_PADDING.x, 0.0f));
+    void PresetGroupsTab::openCreatePresetGroupPanel() {
+        createPresetGroupPanel.openNew("Create Preset Group", "CreatePresetGroupPanel", dataManager, wsSymbolFont);
+        createPresetGroupPanel.get()->focus();
 
-        for (const PresetGroup& group : presetGroups) {
-            ImGui::TableNextRow();
+        createPresetGroupPanel.get()->onCancel([&]() {
+            createPresetGroupPanel.close();
+        });
 
-            ImGui::TableNextColumn();
+        createPresetGroupPanel.get()->onCreate([&](const PresetGroup& presetGroup) {
+            dataManager.addPresetGroup(presetGroup);
+            createPresetGroupPanel.close();
+        });
+    }
 
-            constexpr float selectableHeight = 60.0f;
-            ImVec2 pos = ImGui::GetCursorScreenPos();
-            ImGui::Selectable(("##Selectable_" + group.data->name).c_str(), false, 0, ImVec2(0.0f, selectableHeight));
+    void PresetGroupsTab::openEditPresetGroupPanel(const std::string& presetGroupUUID) {
+        editPresetGroupPanel.openNew(presetGroupUUID, "Edit Preset Group", "EditPresetGroupPanel", dataManager, wsSymbolFont);
+        editPresetGroupPanel.get()->focus();
 
-            // Group name... floating because imgui has no vertical alignment STILL :(
-            ImVec2 labelSize = ImGui::CalcTextSize(group.data->name.c_str());
-            ImVec2 labelPos;
-            labelPos.x = pos.x + ImGui::GetStyle().ItemSpacing.x;
-            labelPos.y = pos.y + (selectableHeight - labelSize.y) * 0.5f;
+        editPresetGroupPanel.get()->onDelete([&](const std::string& presetGroupUUID) {
+            dataManager.deletePresetGroup(presetGroupUUID);
+            editPresetGroupPanel.close();
+        });
 
-            ImGui::GetWindowDrawList()->AddText(labelPos, ImGui::GetColorU32(ImGuiCol_Text), group.data->name.c_str());
+        editPresetGroupPanel.get()->onCancel([&]() {
+            editPresetGroupPanel.close();
+        });
 
-            // Similarly for preset count
-            const size_t nPresets = group.data->presets.size();
-            std::string rightText = std::to_string(nPresets);
-            if (nPresets == 0) rightText = "Empty";
-            else if (nPresets == 1) rightText += " Preset";
-            else rightText += " Presets";
-
-            ImVec2 rightTextSize = ImGui::CalcTextSize(rightText.c_str());
-            float contentRegionWidth = ImGui::GetContentRegionAvail().x;
-            float cursorPosX = ImGui::GetCursorScreenPos().x + contentRegionWidth - rightTextSize.x - ImGui::GetStyle().ItemSpacing.x;
-
-            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-            ImGui::GetWindowDrawList()->AddText(ImVec2(cursorPosX, labelPos.y), ImGui::GetColorU32(ImGuiCol_Text), rightText.c_str());
-            ImGui::PopStyleColor();
-        }
-
-        ImGui::EndTable();
-        ImGui::PopStyleVar(1);
-	}
-
-	void PresetGroupsTab::drawPopouts() {};
+        editPresetGroupPanel.get()->onUpdate([&](const std::string& presetGroupUUID, PresetGroup presetGroup) {
+            dataManager.updatePresetGroup(presetGroupUUID, presetGroup);
+            editPresetGroupPanel.close();
+        });
+    }
 
 }
--- src/sumire/gui/prototypes/gui/tabs/preset_groups/preset_groups_tab.hpp
+++ src/sumire/gui/prototypes/gui/tabs/preset_groups/preset_groups_tab.hpp
@@ -1,6 +1,9 @@
 #pragma once 
 
 #include <sumire/gui/prototypes/gui/tabs/i_tab.hpp>
+#include <sumire/gui/prototypes/gui/panels/unique_panel.hpp>
+#include <sumire/gui/prototypes/gui/panels/create_preset_group_panel.hpp>
+#include <sumire/gui/prototypes/gui/panels/edit_preset_group_panel.hpp>
 
 #include <imgui.h>
 
@@ -8,12 +11,26 @@ namespace kbf {
 
 	class PresetGroupsTab : public iTab {
 	public:
+		PresetGroupsTab(
+			KBFDataManager& dataManager,
+			ImFont* wsSymbolFont = nullptr
+		) : iTab(), dataManager{ dataManager }, wsSymbolFont{ wsSymbolFont } {}
+
+		void setSymbolFont(ImFont* font) { wsSymbolFont = font; }
 
 		void draw() override;
 		void drawPopouts() override;
 
 	private:
+		void drawPresetGroupList();
 
+		UniquePanel<CreatePresetGroupPanel> createPresetGroupPanel;
+		UniquePanel<EditPresetGroupPanel> editPresetGroupPanel;
+		void openCreatePresetGroupPanel();
+		void openEditPresetGroupPanel(const std::string& presetUUID);
+
+		KBFDataManager& dataManager;
+		ImFont* wsSymbolFont;
 	};
 
 }
