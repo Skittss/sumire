16021fdb04630325de1c702e838165b4689af0c1
Skittss <skittss@outlook.com>
2025-08-04 18:20:43 +0100

    info popups support for multiple buttons

--- src/sumire/gui/prototypes/gui/panels/info_popup_panel.cpp
+++ src/sumire/gui/prototypes/gui/panels/info_popup_panel.cpp
@@ -7,8 +7,10 @@ namespace kbf {
 	InfoPopupPanel::InfoPopupPanel(
 		const std::string& label,
 		const std::string& strID,
-		const std::string& message
-	) : iPanel(label, strID), message{ message } {}
+		const std::string& message,
+		const std::string& okLabel,
+		const std::string& cancelLabel
+	) : iPanel(label, strID), message{ message }, okLabel{ okLabel }, cancelLabel{ cancelLabel} {}
 
 	bool InfoPopupPanel::draw() {
 		bool open = true;
@@ -21,14 +23,39 @@ namespace kbf {
 		ImGui::PopTextWrapPos();
 
 		ImGui::Spacing();
+		ImGui::Spacing();
 
-		// Center button
-		float windowWidth = ImGui::GetContentRegionAvail().x;
-		float buttonWidth = ImGui::CalcTextSize("OK").x; // + ImGui::GetStyle().FramePadding.x * 2;
-		ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f);
-		if (ImGui::Button("OK")) {
-			INVOKE_OPTIONAL_CALLBACK(okCallback);
-			open = false;
+		// OK button only
+		if (cancelLabel.empty()) {
+			float windowWidth = ImGui::GetContentRegionAvail().x;
+			float buttonWidth = ImGui::CalcTextSize(okLabel.c_str()).x; // + ImGui::GetStyle().FramePadding.x * 2;
+			ImGui::SetCursorPosX((windowWidth - buttonWidth) * 0.5f);
+			if (ImGui::Button(okLabel.c_str())) {
+				INVOKE_OPTIONAL_CALLBACK(okCallback);
+				open = false;
+			}
+		}
+		else {
+			constexpr float framePadding = 8.0f;
+			float windowWidth = ImGui::GetContentRegionAvail().x;
+			float okWidth = ImGui::CalcTextSize(okLabel.c_str()).x + 2.0f * framePadding;
+			float cancelWidth = ImGui::CalcTextSize(cancelLabel.c_str()).x + 2.0f * framePadding;
+			float totalWidth = okWidth + cancelWidth + ImGui::GetStyle().ItemSpacing.x;
+
+			ImGui::SetCursorPosX((windowWidth - totalWidth) * 0.5f);
+			ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.15f, 0.15f, 0.15f, 1.0f));
+			ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.20f, 0.20f, 0.20f, 1.0f));
+			ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.10f, 0.10f, 0.10f, 1.0f));
+			if (ImGui::Button(cancelLabel.c_str())) {
+				INVOKE_OPTIONAL_CALLBACK(cancelCallback);
+				open = false;
+			}
+			ImGui::SameLine();
+			ImGui::PopStyleColor(3);
+			if (ImGui::Button(okLabel.c_str())) {
+				INVOKE_OPTIONAL_CALLBACK(okCallback);
+				open = false;
+			}
 		}
 
 		ImGui::End();
--- src/sumire/gui/prototypes/gui/panels/info_popup_panel.hpp
+++ src/sumire/gui/prototypes/gui/panels/info_popup_panel.hpp
@@ -13,16 +13,22 @@ namespace kbf {
 		InfoPopupPanel(
 			const std::string& label,
 			const std::string& strID, 
-			const std::string& message);
+			const std::string& message,
+			const std::string& okLabel = "OK",
+			const std::string& cancelLabel = "");
 
 		bool draw() override;
 		void onOk(std::function<void()> callback) { okCallback = callback; }
+		void onCancel(std::function<void()> callback) { cancelCallback = callback; }
 
 	private:
 		constexpr static float wrapWidth = 900.0f; // Width for text wrapping
 		std::string message;
+		std::string okLabel;
+		std::string cancelLabel;
 
 		std::function<void()> okCallback;
+		std::function<void()> cancelCallback;
 	};
 
 }
