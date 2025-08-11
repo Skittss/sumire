0fc7e11b658497de184a27dce89f0f2b417592b6
Skittss <skittss@outlook.com>
2025-08-11 19:44:12 +0100

    Preset reloading

--- src/sumire/gui/prototypes/gui/tabs/settings/settings_tab.cpp
+++ src/sumire/gui/prototypes/gui/tabs/settings/settings_tab.cpp
@@ -5,7 +5,17 @@
 namespace kbf {
 
 	void SettingsTab::draw() {
+		drawTabBarSeparator("Data", "SettingsTabData");
+		ImGui::Spacing();
+		const ImVec2 buttonSize = ImVec2(ImGui::GetContentRegionAvail().x, 0.0f);
+		if (ImGui::Button("Reload Data", buttonSize)) {
+			dataManager.reloadData();
+		}
+		ImGui::SetItemTooltip("Reload Preset Groups, Presets, & Player Overrides.");
+
+		ImGui::Spacing();
 		drawTabBarSeparator("General", "SettingsTabGeneral");
+		ImGui::Spacing();
 		drawTabBarSeparator("Performance", "SettingsTabPerformance");
 	}
 
--- src/sumire/gui/prototypes/gui/tabs/settings/settings_tab.hpp
+++ src/sumire/gui/prototypes/gui/tabs/settings/settings_tab.hpp
@@ -1,17 +1,21 @@
 #pragma once 
 
+#include <sumire/gui/prototypes/data/kbf_data_manager.hpp>
+
 #include <sumire/gui/prototypes/gui/tabs/i_tab.hpp>
 
 namespace kbf {
 
 	class SettingsTab : public iTab {
 	public:
+		SettingsTab(KBFDataManager& dataManager) : dataManager{ dataManager } {}
 
 		void draw() override;
 		void drawPopouts() override;
 		void closePopouts() override;
 
 	private:
+		KBFDataManager& dataManager;
 
 	};
 
